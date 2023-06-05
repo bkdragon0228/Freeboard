862210d141b38de0388d819557ed833885e3808e
bkdragon0228 <rlaqjarb2067@naver.com>
2023-06-05 16:02:43 +0900

    feat : 중고마켓 상품 상세 정보 연결

--- freeboard/src/components/unit/market/detail/MarketItemDetail.container.tsx
+++ freeboard/src/components/unit/market/detail/MarketItemDetail.container.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import { useQuery } from '@apollo/client';
+import { FETCH_USED_ITEM } from './MarketItemDetail.query';
 
 import MarketItemDetailUI from './MarketItemDetail.presenter';
+import { IQuery, IQueryFetchUseditemArgs } from '../../../../commons/types/generated/types';
 
 const MarketItemDetail = () => {
     const router = useRouter()
+    const {data : itemDetail} = useQuery<Pick<IQuery, 'fetchUseditem'>, IQueryFetchUseditemArgs>(FETCH_USED_ITEM, {
+        variables : {
+            useditemId : router.query.id as string
+        }
+    })
 
-    console.log(router.query.id)
+    console.log('detail info' , itemDetail)
     return (
        <MarketItemDetailUI />
     );
 };
 
 export default MarketItemDetail;
+
--- freeboard/src/components/unit/market/detail/MarketItemDetail.query.ts
+++ freeboard/src/components/unit/market/detail/MarketItemDetail.query.ts
@@ -1,1 +1,33 @@
 import { gql } from "@apollo/client";
+
+export const FETCH_USED_ITEM = gql`
+    query FetchUsedItem (
+            $useditemId : ID!
+        ) {
+            fetchUseditem (
+                useditemId : $useditemId
+            ) {
+                _id
+                name
+                remarks
+                contents
+                price
+                tags
+                images
+                pickedCount
+                useditemAddress {
+                    zipcode
+                    address
+                    addressDetail
+                    lat
+                    lng
+                }
+                seller {
+                    _id
+                    name
+                    picture
+                }
+                soldAt
+            }
+    }
+`

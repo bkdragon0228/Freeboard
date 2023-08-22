7282c0b5d2edf55d9f758e6ff6d7fa65862b9a34
bkdragon0228 <rlaqjarb2067@naver.com>
2023-08-22 18:40:39 +0900

    style : style

--- freeboard/src/components/commons/modal/Modal.tsx
+++ freeboard/src/components/commons/modal/Modal.tsx
@@ -1,97 +1,92 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react'
 
 import styled from '@emotion/styled'
 
 interface ModalProps {
-    label : string;
-    bodyContents : JSX.Element;
-    isOpen : boolean;
-    setIsOpen : (boolean: boolean) => void
+  label: string
+  bodyContents: JSX.Element
+  isOpen: boolean
+  setIsOpen: (boolean: boolean) => void
 }
 
 const ModalContainer = styled.div`
-    position: fixed;
-    top : 0;
-    left : 0;
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    background-color: rgba(0,0,0,0.6);
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.6);
 `
 
 const ModalWrapper = styled.div`
-    position: absolute;
-    display: flex;
-    flex-direction: column;
-    row-gap: 1rem;
-    background-color: white;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  row-gap: 1rem;
+  background-color: white;
 `
 const ModalCloseButton = styled.button`
-    position: absolute;
-    top: 20px;
-    right: 20px;
-    width: 50px;
-    height: 50px;
-    border: none;
-    background-color: transparent;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 2rem;
-    border-radius: 50%;
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  width: 50px;
+  height: 50px;
+  border: none;
+  background-color: transparent;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2rem;
+  border-radius: 50%;
 
-    &:hover {
-        background-color: lightgray;
-    }
+  &:hover {
+    background-color: lightgray;
+  }
 `
 const ModalLabel = styled.div`
-    width: 100%;
-    padding: 32px;
-    font-size: 2rem;
-    font-weight: 700;
-    margin: 0 auto;
-    text-align: center;
-    border-bottom: 1px solid lightgray;
+  width: 100%;
+  padding: 32px;
+  font-size: 2rem;
+  font-weight: 700;
+  margin: 0 auto;
+  text-align: center;
+  border-bottom: 1px solid lightgray;
 `
 const ModalContents = styled.div`
-    padding : 20px;
-    width: 700px;
-    min-height: 800px;
-    display: flex;
-    flex-direction: column;
-    row-gap: 1rem;
-    background-color: white;
+  padding: 20px;
+  width: 700px;
+  min-height: 800px;
+  display: flex;
+  flex-direction: column;
+  row-gap: 1rem;
+  background-color: white;
 `
 
-const Modal : React.FC<ModalProps> = ({
-    bodyContents,
-    isOpen,
-    setIsOpen,
-    label
+const Modal: React.FC<ModalProps> = ({
+  bodyContents,
+  isOpen,
+  setIsOpen,
+  label,
 }) => {
+  if (!isOpen) {
+    return null
+  }
 
-    if(!isOpen) {
-        return null;
-    }
+  return (
+    <ModalContainer>
+      <ModalWrapper>
+        <ModalCloseButton onClick={() => setIsOpen(false)}>
+          <i className="ri-close-line"></i>
+        </ModalCloseButton>
+        <ModalLabel>{label}</ModalLabel>
+        <ModalContents>{bodyContents}</ModalContents>
+      </ModalWrapper>
+    </ModalContainer>
+  )
+}
 
-    return (
-        <ModalContainer >
-            <ModalWrapper>
-                <ModalCloseButton onClick={() => setIsOpen(false)}>
-                     <i className="ri-close-line"></i>
-                </ModalCloseButton>
-                <ModalLabel>
-                    {label}
-                </ModalLabel>
-                <ModalContents>
-                    {bodyContents}
-                </ModalContents>
-            </ModalWrapper>
-        </ModalContainer>
-    );
-};
-
-export default Modal;
+export default Modal
